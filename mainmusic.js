72e886343626a1c675c70738f6a934dbf30d51d4
Giacomo Rossettini <rossegiac@gmail.com>
2024-10-01 15:08:28 +0200

    fix mainmusic

--- mainmusic.js
+++ mainmusic.js
@@ -61,4 +61,33 @@ const cantautori = [
 const cantautoriPerLink = cantautori.map((item) => item.replace(/\s+/g, "").toLocaleLowerCase());
 
 const apiLink = `https://striveschool-api.herokuapp.com/api/deezer/search?q=${cantautoriPerLink[Math.floor(Math.random() * cantautoriPerLink.length)]}`;
-console.log(apiLink);
+
+fetch(apiLink)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Errore nella domanda al server");
+    }
+    return response.json();
+  })
+  .then((data) => {
+    let randomCantante = Math.floor(Math.random() * data.data.length);
+    //CREAZIONE IMG
+    let mainImage = document.createElement("img");
+    mainImage.src = data.data[randomCantante].artist.picture_medium;
+    mainImage.alt = `${data.data[randomCantante].title_short} picture`;
+    //CREAZIONE DIV CANTANTE
+    let cantanteBox = document.createElement("div");
+    cantanteBox.className = "d-flex flex-column justify-content-between overflow-hidden";
+    cantanteBox.innerHTML = `  <h6>ALBUM</h6>
+  <h1 class="text-break">${data.data[randomCantante].title}</h1>
+  <h5 class="mb-0">${data.data[randomCantante].artist.name}</h5>
+  <h5>Ascolta il nuovo singolo targato ${data.data[randomCantante].artist.name}</h5>
+  <div class="d-flex gap-3">
+    <button id="playbtn" class="rounded-pill px-4 py-2 border">Play</button>
+    <button id="salvabtn" class="rounded-pill px-4 py-2 border text-white">Salva</button>
+  </div>`;
+    mainMusicBox.append(mainImage, cantanteBox);
+  })
+  .catch((err) => {
+    console.log("Errore" + err);
+  });
