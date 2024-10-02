24e2648d866d266d07682f51588791a852b35f1e
Kateryna <katiaemme96@yahoo.it>
2024-10-02 11:06:30 +0200

    aggiunta

--- tabella.js
+++ tabella.js
@@ -1,13 +1,16 @@
 //al click dell'album[id]
 // prendi l'album[id] e un tot/tutte delle sue canzoni
 
-{
-  /* <div class="row">
-                    <div class="col col-4 d-flex gap-2">
+const songsContainer = document.getElementById("songs-container");
+const row = document.createElement("div");
+row.classList.add("row");
+row.innerHTML = ` 
+
+ <div class="col col-4 d-flex gap-2">
                       <div class="text-white-50">1</div>
                       <div>
-                        <h6 class="mb-0">Montanelli - Intro</h6>
-                        <p class="text-white-50">Pinguini Tattici Nucleari</p>
+                        <h6 class="mb-0">${title}</h6>
+                        <p class="text-white-50">${artist.name}</p>
                       </div>
                     </div>
                     <div class="col col-4 d-flex justify-content-end text-white-50">
@@ -16,5 +19,7 @@
                     <div class="col col-4 d-flex justify-content-end text-white-50">
                       <p>1:28</p>
                     </div>
-                  </div> */
-}
+
+
+`;
+songsContainer.appendChild(row);
