c2b492b47f68cc51bad002f5d2a7dc40115ebec3
Kateryna <katiaemme96@yahoo.it>
2024-10-02 17:59:22 +0200

    un po di css risolto

--- mainmusic.js
+++ mainmusic.js
@@ -156,9 +156,35 @@ if (albumId) {
       let selectedAlbum = document.createElement("div");
       selectedAlbum.className = "col col-lg-9 w-100";
       selectedAlbum.innerHTML = `
-        <div class="mainbox w-100 col col-12 col-lg-7 text-white overflow-auto p-0 Gianmarco">
+        <div class="mainbox w-100 col col-12 col-lg-7 text-white overflow-hidden p-0 Gianmarco">
+        
           <div class="bgmain h-50 position-relative z-0">
+
+           <div class="col d-flex gap-2 justify-content-between align-content-center pb-4">
+                <div class="d-flex gap-2">
+                  <i class="bi bi-arrow-left-circle-fill opacity-75 text-dark ps-3 pe-1 fs-2"></i>
+                  <i class="bi bi-arrow-right-circle-fill opacity-75 text-dark fs-2"></i>
+                </div>
+
+                <a
+                  class="nav-link dropdown-toggle bg-black rounded-4 opacity-75 px-1 pe-1 mt-1 me-1 d-flex align-items-center"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Gianmarco Arzanese
+                </a>
+                <ul class="dropdown-menu opacity-50">
+                  <li><a class="dropdown-item" href="#">Action</a></li>
+                  <li><a class="dropdown-item" href="#">Another action</a></li>
+                  <li>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                  </li>
+                </ul>
+              </div>
             <div class="row m-0 pb-3 ps-3">
+            
               <div class="col col-lg-3 shadow-lg p-0 ps-1">
                 <img src="${data.cover_medium}" alt="album" style="width: 100%" />
               </div>
@@ -175,9 +201,10 @@ if (albumId) {
       mainMusicBox.appendChild(selectedAlbum);
       // GENERAZIONE TABELLA LISTA CANZONI ALBUM
       let container = document.createElement("div");
-      container.className = "container position-absolute containerPlay p-3 m-0 w-100";
-      container.style.top = "15rem";
+      container.className = "container position-absolute containerPlay p-3  w-100";
+      container.style.top = "18rem";
       container.style.bottom = "0";
+      container.style.overflow = "auto";
       container.innerHTML = `<div class="col d-flex align-items-center gap-2">
                   <a class="nav-link" href="#"><i class="bi bi-play-circle-fill text-success fs-1"></i></a>
                   <a class="nav-link" href="#"><i class="bi bi-heart"></i></a>
