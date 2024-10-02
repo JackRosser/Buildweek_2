b75d73602f003104c5c7acfc2bcd1aee4d9bcb80
Kateryna <katiaemme96@yahoo.it>
2024-10-02 11:06:18 +0200

    aggiunto una pagina js della tabella delle canzoni che si collega all'evento click sull'album (2nd page)

---
+++ tabella.js
@@ -0,0 +1,20 @@
+//al click dell'album[id]
+// prendi l'album[id] e un tot/tutte delle sue canzoni
+
+{
+  /* <div class="row">
+                    <div class="col col-4 d-flex gap-2">
+                      <div class="text-white-50">1</div>
+                      <div>
+                        <h6 class="mb-0">Montanelli - Intro</h6>
+                        <p class="text-white-50">Pinguini Tattici Nucleari</p>
+                      </div>
+                    </div>
+                    <div class="col col-4 d-flex justify-content-end text-white-50">
+                      <p>12.000</p>
+                    </div>
+                    <div class="col col-4 d-flex justify-content-end text-white-50">
+                      <p>1:28</p>
+                    </div>
+                  </div> */
+}
